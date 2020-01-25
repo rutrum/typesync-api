4a2daf69b7ccf61411cbc3b6ccba7c2fe28910e9
David Purdum <purdum41@gmail.com>
2020-01-25 14:42:48 -0500

    Try catch is for babies

--- Database.py
+++ Database.py
@@ -4,20 +4,20 @@ from mysql.connector import errorcode
 pass_file = open("root_pass.txt")
 root_pass = pass_file.read().strip()
 
-try:
-    cnx = mysql.connector.connect(
-        host='35.193.10.101',
-        database='typingtest',
-        user='root',
-        password=root_pass
-    )
-except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Something is wrong with your user name or password")
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("Database does not exist")
-    else:
-        print(err)
+# try:
+cnx = mysql.connector.connect(
+    host='35.193.10.101',
+    database='typingtest',
+    user='root',
+    password=root_pass
+)
+# except mysql.connector.Error as err:
+#     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+#         print("Something is wrong with your user name or password")
+#     elif err.errno == errorcode.ER_BAD_DB_ERROR:
+#         print("Database does not exist")
+#     else:
+#         print(err)
 
 
 def add_new_score(name, geniusID, time, score, spotifyID):
